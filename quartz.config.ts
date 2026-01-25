85d829caf9a4543d01a69ae91ade7225bf6c78d0
LJY <7yuny1@gmail.com>
2026-01-25 13:37:27 +0800

    Update site title to Kevin's Note

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Kevin's Note",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
