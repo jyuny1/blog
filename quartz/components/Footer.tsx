51277b885658b2692d98ed059f5c52ecbee68101
LJY <7yuny1@gmail.com>
2026-01-25 20:19:15 +0800

    style: update footer with custom copyright and remove default links

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,18 +15,12 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year} LJY Photography
         </p>
         <p>
           本站內容採用 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hant">CC BY-NC-SA 4.0 國際授權條款</a> 分享
         </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+        
       </footer>
     )
   }
