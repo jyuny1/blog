9fa72b50508f19d5d44a0cfc183f969a21c2d417
LJY <7yuny1@gmail.com>
2026-02-06 02:11:46 +0800

    Architectural fix for LightGallery SPA: use Quartz lifecycle injection

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,5 +1,7 @@
 // @ts-ignore
 import clipboardScript from "./scripts/clipboard.inline"
+// @ts-ignore
+import lightgalleryScript from "./scripts/lightgallery.inline"
 import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
@@ -7,7 +9,10 @@ const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
   return <div id="quartz-body">{children}</div>
 }
 
-Body.afterDOMLoaded = clipboardScript
+Body.afterDOMLoaded = `
+${clipboardScript}
+${lightgalleryScript}
+`
 Body.css = clipboardStyle
 
 export default (() => Body) satisfies QuartzComponentConstructor
--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -99,7 +99,6 @@ export default (() => {
         <script src="https://cdn.jsdelivr.net/npm/lightgallery@2.7.2/plugins/thumbnail/lg-thumbnail.min.js" defer data-persist="true"></script>
         <script src="https://cdn.jsdelivr.net/npm/lightgallery@2.7.2/plugins/zoom/lg-zoom.min.js" defer data-persist="true"></script>
         <script src="https://cdn.jsdelivr.net/npm/lightgallery@2.7.2/plugins/fullscreen/lg-fullscreen.min.js" defer data-persist="true"></script>
-        <script src="/static/custom_lg.js" defer data-persist="true"></script>
         {additionalHead.map((resource) => {
           if (typeof resource === "function") {
             return resource(fileData)
